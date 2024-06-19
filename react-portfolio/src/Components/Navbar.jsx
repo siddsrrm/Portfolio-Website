d7193b80c9547c1d26472cb1727e2e32a6febafd
SIDDHARTH-HP\siddharth <siddsrrm@gmail.com>
2024-06-18 20:56:52 -0400

    created navbar layout

---
+++ react-portfolio/src/Components/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import styles from "./Navbar.module.css"
+
+export const Navbar = () => {
+  return (
+    <nav className={styles.navbar}> 
+        <ul className={styles.navItems}>
+            <li>
+                <a href="#home">Home</a>
+            </li>
+            <li>
+                <a href="#about">About</a>
+            </li>
+            <li>
+                <a href="#expierence">Experience</a>
+            </li>
+            <li>
+                <a href="#contact">Contact</a>
+            </li> 
+        </ul>            
+    </nav>
+  )
+}
